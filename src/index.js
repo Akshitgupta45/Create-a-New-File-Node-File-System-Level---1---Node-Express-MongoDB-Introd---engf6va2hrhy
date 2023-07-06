415f5fa994ad00dd095e85306d9046a133949874
Akshitgupta45 <akshit.rke@gmail.com>
2023-07-06 21:04:04 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
-const writeFile = () => {
+const writeFile = async (fileName, fileContent) => {
 	// write code here
 	// dont change function name
-
-	fs.writeFile(fileName, fileContent, (error) => {
-		if (error) {
-			console.log("Error while creating file");
-		}
-		console.log("file is created");
-	});
+	try {
+		await fs.writeFile(fileName, fileContent);
+		console.log(`File "${fileName}" created successfully.`);
+	} catch (error) {
+		console.error(`Error creating file: ${fileName}`);
+	}
 };
-writeFile();
 
 module.exports = { writeFile };
